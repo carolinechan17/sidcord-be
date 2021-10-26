93511a02021f734d699e4b5d4fa7c2d52fb1a413
zulfaza <32305083+zulfaza@users.noreply.github.com>
2021-10-26 23:39:01 +0700

    update midrans env var

--- Midtrans.js
+++ Midtrans.js
@@ -1,7 +1,7 @@
 require("dotenv").config(); // this is important!
 const midtransClient = require("midtrans-client");
 const snap = new midtransClient.Snap({
-  isProduction: process.env.NODE_ENV === "production",
+  isProduction: process.env.MIDTRANS_ENV,
   serverKey: process.env.MIDTRANS_SERVER_KEY,
   clientKey: process.env.MIDTRANS_CLIENT_KEY,
 });
