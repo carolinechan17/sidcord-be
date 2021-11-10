ef295f34a1196c6623632d854bfb623e0d4396a7
zulfaza <32305083+zulfaza@users.noreply.github.com>
2021-11-10 11:01:40 +0700

    tambah route untuk list provinsi dan kota

--- routes/couriers.js
+++ routes/couriers.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const model = require("../models/index");
-
+const RajaOngkir = require("rajaongkir-nodejs").Starter(
+  "40714cc6e20d56f0f752a24a3ab06a35"
+);
 router.post("/", async (req, res) => {
   const { nama, basePrice } = req.body;
   if (!nama || !basePrice)
@@ -37,6 +39,69 @@ router.get("/", async (req, res) => {
   return res.json({
     data: couriers,
   });
+});
+
+//check ongkir
+router.post("/ongkir", function (req, res) {
+  const { kurir, asal, tujuan } = req.body;
+  const params = {
+    origin: asal,
+    destination: tujuan,
+    weight: 1000,
+  };
+
+  let query = RajaOngkir;
+
+  switch (kurir) {
+    case "POS":
+      query = query.getPOSCost(params);
+    case "TIKI":
+      query = query.getTIKICost(params);
+    default:
+      //JNE
+      query = query.getJNECost(params);
+      break;
+  }
+
+  return query
+    .then((result) => {
+      return res.json(result);
+    })
+    .catch((err) => {
+      console.log(err);
+      return res.status(400).json(err);
+    });
+});
+
+router.get("/provinsi/:provinsiId", async (req, res) => {
+  const { provinsiId = null } = req.params;
+  let query = RajaOngkir;
+
+  if (provinsiId) query = query.getProvince(provinsiId);
+  else query = query.getProvinces();
+
+  return query
+    .then((result) => {
+      return res.json(result);
+    })
+    .catch((err) => {
+      console.log(err);
+      return res.status(400).json(err);
+    });
+});
+
+router.get("/kota/:id", async (req, res) => {
+  const idKota = req.params.id;
+  if (idKota) return res.status(401).json({ message: "missing id kota" });
+
+  return RajaOngkir.getCity(idKota)
+    .then((result) => {
+      return res.json(result);
+    })
+    .catch((err) => {
+      console.log(err);
+      return res.status(400).json(err);
+    });
 });
 
 module.exports = router;
